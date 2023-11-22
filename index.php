ddfc923de22814094682582d52e3c06ce4170053
Vazohh <vazohhbtw@gmail.com>
2023-11-22 11:35:24 +0100

    Contraignez l'usage de vos classes

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  *
  * (c) Grégoire Hébert <contact@gheb.dev>
  *
- * For the full copyright and license information, please view the License
+ * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
@@ -41,23 +41,34 @@ class Lobby
     }
 }
 
-class Player
+abstract class AbstractPlayer
 {
-    public function __construct(protected string $name, protected float $ratio = 400.0)
+    public function __construct(public string $name = 'anonymous', public float $ratio = 400.0)
     {
     }
 
+    abstract public function getName(): string;
+
+    abstract public function getRatio(): float;
+
+    abstract protected function probabilityAgainst(self $player): float;
+
+    abstract public function updateRatioAgainst(self $player, int $result): void;
+}
+
+class Player extends AbstractPlayer
+{
     public function getName(): string
     {
         return $this->name;
     }
 
-    private function probabilityAgainst(self $player): float
+    protected function probabilityAgainst(AbstractPlayer $player): float
     {
         return 1 / (1 + (10 ** (($player->getRatio() - $this->getRatio()) / 400)));
     }
 
-    public function updateRatioAgainst(self $player, int $result): void
+    public function updateRatioAgainst(AbstractPlayer $player, int $result): void
     {
         $this->ratio += 32 * ($result - $this->probabilityAgainst($player));
     }
@@ -70,7 +81,7 @@ class Player
 
 class QueuingPlayer extends Player
 {
-    public function __construct(Player $player, protected int $range = 1)
+    public function __construct(AbstractPlayer $player, protected int $range = 1)
     {
         parent::__construct($player->getName(), $player->getRatio());
     }
@@ -86,8 +97,8 @@ class QueuingPlayer extends Player
     }
 }
 
-$greg = new Player('greg', 400);
-$jade = new Player('jade', 476);
+$greg = new Player('greg');
+$jade = new Player('jade');
 
 $lobby = new Lobby();
 $lobby->addPlayers($greg, $jade);
